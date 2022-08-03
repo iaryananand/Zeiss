1249695db71c47166ccc4385333f3d9ee4e259c4
X6AANAN1 <X6AANAN1@IN05N000T9>
2022-08-03 14:53:16 +0530

    Add project files.

---
+++ Zeiss.BL/PayBL.cs
@@ -0,0 +1,52 @@
+﻿using Zeiss.DataAccess;
+
+namespace Zeiss.BL
+{
+    public class PayBL
+    {
+        public int GetBasicPay(string empID, Int16 Month, Int16 Year)
+        {
+            PayDA payDa = new PayDA();
+            int basicPay = payDa.GetBasicPay(empID, Month, Year);
+            return basicPay;        
+        }
+
+        public int CalculateDA(string empID, Int16 Month, Int16 Year)
+        {
+            int basicPay = GetBasicPay(empID, Month, Year);
+            int da = 0;
+            if (basicPay > 25000)
+                da = basicPay * 30 / 100;
+            else
+                da = basicPay * 40 / 100;
+            return da;
+        }
+
+       
+        
+    }
+    public class EmployeeBL
+    {
+        public string empID;
+        public string FirstName;
+        public string LastName;
+        public string DOJ;
+
+        public EmployeeBL GetEmpDetails(string empID)
+        {
+            // get emp details
+            EmployeeDA empD1 = new EmployeeDA();
+            empD1 = empD1.GetEmpDetails(empID);
+
+            // Populate BL object from DA object
+            EmployeeBL empB1 = new EmployeeBL();
+            empB1.EmpID = empD1.empID;
+            empB1.FirstName = empD1.FirstName;
+            empB1.LastName = empD1.LastName;
+            empB1.DOJ = empD1.DOJ;
+
+            return empB1;
+
+        }
+    }
+}
---
+++ Zeiss.PaySlip/PaySlip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zeiss.BL;
+
+namespace Zeiss.PaySlip
+{
+   
+    public class PaySlip
+    {
+        string EmpID;
+        string EmpFirstName;
+        string EmpLastName;
+        string Department;
+        DateTime DOJ;
+        short Month;
+        short Year;
+        int BasicPay;
+        int DA;
+        int HRA;
+        int LTA;
+        int PF;
+        int ProfTax;
+        int IncomeTax;
+        int Loan;
+
+        public void PrintPaySlip(string empID, short Month, short Year)
+        {
+            Console.WriteLine("PaySlip for" + Year.ToString() + "-" + Month.ToString());
+
+            EmployeeBL empBL = new EmployeeBL();
+            empBL = empBL.GetEmpDetails("M100732");
+            Console.WriteLine(empBL.empID + "  " + empBL.FirstName + " " + empBL.LastName);
+            Console.WriteLine("Date of Joining: " + empBL.DOJ);
+        }
+    }
+}
